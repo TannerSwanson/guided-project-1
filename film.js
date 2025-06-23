298c27d183f03c07b652f7522702331a3a7863d5
Tanner Swanson <TSWANSO3@travelers.com>
2025-06-23 15:25:38 -0400

    Working film title retrieval.

---
+++ film.js
@@ -0,0 +1,56 @@
+let nameH1;
+let birthYearSpan;
+let heightSpan;
+let massSpan;
+let filmsDiv;
+let planetDiv;
+const baseUrl = `http://localhost:9001/api`;
+
+// Runs on page load
+addEventListener('DOMContentLoaded', () => {
+  filmH1 = document.querySelector('h1#film');
+  const sp = new URLSearchParams(window.location.search)
+  const id = sp.get('id')
+  console.log(id);
+  getFilm(id)
+});
+
+async function getFilm(id) {
+  let character;
+  try {
+    film = await fetchFilm(id)
+    console.log(film)
+    film.characters = await fetchCharacters(film)
+    film.planets = await fetchPlanets(film)
+  }
+  catch (ex) {
+    console.error(`Error reading film ${id} data.`, ex.message);
+  }
+  renderFilm(film);
+
+}
+async function fetchFilm(id) {
+  let filmUrl = `${baseUrl}/films/${id}`;
+  console.log(filmUrl);
+  return await fetch(filmUrl)
+    .then(res => res.json())
+}
+
+async function fetchCharacters(id) {
+    let characterUrl = `${baseUrl}/films/${id}/characters`;
+    return await fetch(characterUrl)
+        .then(res => res.json())
+}
+
+async function fetchPlanets(id) {
+    let planetUrl = `${baseUrl}/films/${id}/planets`;
+    return await fetch(planetUrl)
+        .then(res => res.json())
+}
+
+const renderFilm = film => {
+    console.log(film.title)
+    document.title = `SWAPI - ${film?.title}`;  // Just to make the browser tab say their name
+    filmH1.textContent = film.title;
+
+}
