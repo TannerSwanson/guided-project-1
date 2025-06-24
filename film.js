58e26ed8afda39dc8618d8e58259726b75363369
Tanner Swanson <TSWANSO3@travelers.com>
2025-06-24 09:14:34 -0400

    Added additional film information

--- film.js
+++ film.js
@@ -1,13 +1,33 @@
 let filmH1;
 let charactersUl;
 let planetsUl;
+let dirSp;
+let prodSp;
+let dateSp;
+let epSp;
+let opening_crawlP;
 const baseUrl = `http://localhost:9001/api`;
+
+/*
+ <section id="generalInfo">
+      <p>Director: <span id="director"></span></p>
+      <p>Episode: <span id="episode"></span></p>
+      <p>Release Date: <span id="date"></span></p>
+      <p>Producers: <span id="producer"></span></p>
+*/
+
+
 
 // Runs on page load
 addEventListener('DOMContentLoaded', () => {
   filmH1 = document.querySelector('h1#film');
   charactersUl = document.querySelector('#characters>ul');
   planetsUl = document.querySelector('#planets>ul');
+  dirSp = document.querySelector('span#director');
+  prodSp = document.querySelector('span#producer');
+  dateSp = document.querySelector('span#date');
+  epSp = document.querySelector('span#episode');
+  opening_crawlP = document.querySelector('p#opening_crawl');
   const sp = new URLSearchParams(window.location.search)
   const id = sp.get('id')
   getFilm(id)
@@ -17,6 +37,7 @@ async function getFilm(id) {
   let film;
   try {
     film = await fetchAPI(`/films/${id}`)
+    console.log(film)
     film.characters = await fetchAPI(`films/${id}/characters`)
     film.planets = await fetchAPI(`films/${id}/planets`)
     renderFilm(film);
@@ -35,7 +56,14 @@ async function fetchAPI(extraUrl) {
 const renderFilm = film => {
     console.log(film.title)
     document.title = `SWAPI - ${film?.title}`;
+    console.log(film.direct)
     filmH1.textContent = film.title;
+    dirSp.textContent = film.director;
+    prodSp.textContent = film.producer;
+    dateSp.textContent = film.release_date;
+    epSp.textContent = film.episode_id;
+    opening_crawlP.textContent = film.opening_crawl; 
+
 
     const charList = film.characters.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
     charactersUl.innerHTML = charList.join("");
